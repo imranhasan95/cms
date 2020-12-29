3ac180998bb9047103966f220bc0b3aa5ca4544e
imranhasan95 <imranhasan1811@gmail.com>
2020-12-29 13:04:52 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,121 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', 'HomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+/** This is the main app route [Chatify Messenger]
+*/
+Route::get('/messages', 'MessagesController@index')->name(config('chatify.path'));
+
+/**
+ *  Fetch info for specific id [user/group]
+ */
+Route::post('/idInfo', 'MessagesController@idFetchData');
+
+/**
+ * Send message route
+ */
+Route::post('/sendMessage', 'MessagesController@send')->name('send.message');
+
+/**
+ * Fetch messages
+ */
+Route::post('/fetchMessages', 'MessagesController@fetch')->name('fetch.messages');
+
+/**
+ * Download attachments route to create a downloadable links
+ */
+Route::get('/download/{fileName}', 'MessagesController@download')->name(config('chatify.attachments.route'));
+
+/**
+ * Authintication for pusher private channels
+ */
+Route::post('/chat/auth', 'MessagesController@pusherAuth')->name('pusher.auth');
+
+/**
+ * Make messages as seen
+ */
+Route::post('/makeSeen', 'MessagesController@seen')->name('messages.seen');
+
+/**
+ * Get contacts
+ */
+Route::post('/getContacts', 'MessagesController@getContacts')->name('contacts.get');
+
+/**
+ * Update contact item data
+ */
+Route::post('/updateContacts', 'MessagesController@updateContactItem')->name('contacts.update');
+
+
+/**
+ * Star in favorite list
+ */
+Route::post('/star', 'MessagesController@favorite')->name('star');
+
+/**
+ * get favorites list
+ */
+Route::post('/favorites', 'MessagesController@getFavorites')->name('favorites');
+
+/**
+ * Search in messenger
+ */
+Route::post('/search', 'MessagesController@search')->name('search');
+
+/**
+ * Get shared photos
+ */
+Route::post('/shared', 'MessagesController@sharedPhotos')->name('shared');
+
+/**
+ * Delete Conversation
+ */
+Route::post('/deleteConversation', 'MessagesController@deleteConversation')->name('conversation.delete');
+
+/**
+ * Delete Conversation
+ */
+Route::post('/updateSettings', 'MessagesController@updateSettings')->name('avatar.update');
+
+/**
+ * Set active status
+ */
+Route::post('/setActiveStatus', 'MessagesController@setActiveStatus')->name('activeStatus.set');
+
+
+/*
+* [Group] view by id
+*/
+Route::get('/group/{id}', 'MessagesController@index')->name('group');
+
+/*
+* user view by id.
+* Note : If you added routes after the [User] which is the below one,
+* it will considered as user id.
+*
+* e.g. - The commented routes below :
+*/
+// Route::get('/route', function(){ return 'Munaf'; }); // works as a route
+Route::get('/messages/{id}', 'MessagesController@index')->name('user');
+// Route::get('/route', function(){ return 'Munaf'; }); // works as a user id
